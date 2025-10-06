3966a16f62b5a35cc12ae60a9ad53b22b2ee7a88
Dipesh Sharma <dipeshsharmagautam@outlook.com>
2025-10-06 09:54:23 +0530

    add the most optimal solution of the longest subarray, with getting right and left value ok

---
+++ Two Pointer Sliding Window/Longest Subarray/optimal.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int>ConstantWindow(int arr[], int n, int k){
+  int l=0, r=0;
+  int maxStart=0, maxEnd=0, sum=0;
+  vector<int>res;
+  while(r<n){
+    sum+=arr[r];
+    while(sum>k){
+      sum-=arr[l];
+      l++;
+    }
+    if(sum<=k){
+      maxStart=max(maxStart, l);
+      maxEnd=max(maxEnd, r);
+      r++; 
+    }
+  }
+  for(int i=maxStart-1;i<=maxEnd;i++)
+    res.push_back(arr[i]);
+  return res;
+}
+int main(){
+  int arr[]={12, 3, 15, 10, 6, 8, 4, 1, 12, 9, 14, 18, 6};
+  int n=sizeof(arr)/sizeof(arr[0]);
+  int maxnum=35;
+  vector<int>res=ConstantWindow(arr, n, maxnum);
+  for(int i=0;i<res.size();i++){
+    cout<<res[i]<<" ";
+}
+return 0;
+}
