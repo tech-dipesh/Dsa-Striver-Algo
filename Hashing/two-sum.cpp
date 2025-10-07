9e7718fd44da3426f76c510bce3d3ab4257990b7
Dipesh Sharma <dipeshsharmagautam@outlook.com>
2025-10-07 12:00:29 +0530

    add the two sum problem solution with the hashmap getting the target value, if that possible just return those indieces

---
+++ Hashing/two-sum.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+vector<int>twoSum(int arr[], int n, int target){
+// vector<int> twoSum(int arr[], int n, int target){
+  // vector<int>res;
+  // int res[2];
+  unordered_map<int, int>mp;
+  for(int i=0;i<n;i++){
+    int sub=target-arr[i];
+    if(mp.find(sub)!=mp.end()){
+      // example is: i become, 5 index: 2, and now mp[2], so mp index 2 have value of: 0 which is the indesx 
+      int firsthashmapindex=mp[arr[sub]];
+      return {i, firsthashmapindex};
+      // res.push_back(i);
+      // res.push_back(nextvalue);
+      // return res;
+    }
+    mp[arr[i]]=i;
+  }
+  // return res;
+  return {};
+}
+
+int main(){
+  int arr[]={2, 8, 5, 3, 9, 11, 4};
+  int target=7;
+  int size=sizeof(arr)/sizeof(arr[0]);
+  cout<<twoSum(arr, size, target);
+}
