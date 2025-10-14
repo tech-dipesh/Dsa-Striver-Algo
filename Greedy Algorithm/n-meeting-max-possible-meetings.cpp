fb5bc3bdb2f262a5afe191d547e33ebf1ba64b52
Dipesh Sharma <dipeshsharmagautam@outlook.com>
2025-10-14 18:20:50 +0530

    update the inor changs on teh n meetinc doe

--- Greedy Algorithm/n-meeting-max-possible-meetings.cpp
+++ Greedy Algorithm/n-meeting-max-possible-meetings.cpp
@@ -1,5 +1,10 @@
+// now the answer should be that the possible meeting that can held, this is kind of almost similar question of the job sequence.
+// with from teh if we do: (0, 5) that can be only 2 so that will not possible we' to make the maximum.
+// max possible is: (1, 2), (3, 4), (5, 7) (8, 9)
+// it also shouldn't be overlap like: if we finished job on 5 we can't start on 5.
 #include <iostream>
 #include <algorithm>
+#include <queue>
 using namespace std;
 struct Meet{
   int id;
@@ -20,29 +25,31 @@ int main(){
     {6, 8, 9}
   };
 
-  int n=6;
+  int n=sizeof(arr)/sizeof(arr[0]);
 
-  // now the answer should be that the possible meeting that can held, this is kind of almost similar question of the job sequence.
-  // with from teh if we do: (0, 5) that can be only 2 so that will not possible we' to make the maximum.
-  // max possible is: (1, 2), (3, 4), (5, 7) (8, 9)
-  // it also shouldn't be overlap like: if we finished job on 5 we can't start on 5.
-
+  // use stack ds for storign end tme and check any overlap of new element start time, the first start will always be the smallest ending time.
   sort(arr, arr+n, Sort);
-
-  int count=0, startmeeting=0;
-
-
-
-  // our approach is to preserver the stack ds for teh storing the ending time for checking a any overlap, and counting the size for the final result.
-
-  // start with the shortest possible ending time and store that ending time on the stack, now on the next step get the starting value from the array where that is greater then the stack top until not get the top.
-  // do that if we found out that the new starting time is equal or smaller just skip that and go to next.
+  int count=1, endingMeetingTime=arr[0].end;
+  queue<int>st;
+  int res[n];
   
-
-  for(int i=0;i<n;i++){
-     cout<<arr[i].end<<" "<<endl<<arr[i].start<<endl<<" "<<arr[i].id;
+  // st.push(arr[0].id);
+  res[0]=arr[0].id;
+  int j=1;
+  for(int i=1;i<n;i++){
+    if(arr[i].start>endingMeetingTime){
+      count++;
+      endingMeetingTime=arr[i].end;
+      // st.push(arr[i].id);
+      res[j++]=arr[i].id;
+    }
   }
 
+  cout<<"total count is: "<<count<<endl;
+  
+  for(int i=0;i<j;i++){
+    cout<<"All Id is: "<<res[i]<<" ";
+  }
 
 return 0;
 }
