727fcf305f019f06098af4a0b266a982efb34550
Dipesh Sharma <dipeshsharmagautam@outlook.com>
2025-10-29 10:21:33 +0530

    add the merge sort and recursion sort practice files

---
+++ Practice/recursion-practice.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <bits/stdc++.h>
+ 
+using namespace std;
+int main(){
+ multiset<int>ms;
+ ms.insert(238);
+ ms.insert(83);
+ ms.insert(52);
+ ms.insert(82);
+ 
+
+// cout<<ms[;   
+  for(int i:ms){
+    cout<<i<<" ";
+  }
+return 0;
+}
