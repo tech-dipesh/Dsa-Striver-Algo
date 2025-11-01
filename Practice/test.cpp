33d567662dee13493629892ecd57cbc51c290310
Dipesh Sharma <dipeshsharmagautam@outlook.com>
2025-11-01 17:35:34 +0530

    wreit down the practice file on the practicce folder

--- Practice/test.cpp
+++ Practice/test.cpp
@@ -1,30 +1,55 @@
+// count inversion solution: (5, 3) (5, 2), (5, 4)
 #include <iostream>
 #include <vector>
 using namespace std;
 
-  void moveZeroes(vector<int>& nums) {
-        int count=0;
-        vector<int>vt;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==0){
-                vt.push_back(nums[i]);
-            }
-            else{
-              nums[i]=nums[i];
-              count++;
-            }
-        }
 
-  for(int i=count;i<vt.size();i++){
-      nums[i]=vt[i];
-      
-  }
+void mergetwoSortedArray(int low, int medium, int high, vector<int>&res){
+  vector<int>temp;
+  int i=low, j=medium;
+  while(i<=medium && j<=high){
+      if(res[i]<res[j]){
+          temp.push_back(res[i]);
+          i++;
+      }
+      else{
+        temp.push_back(res[j]);
+        j++;
+      }
   }
 
+  // there might still some element left:
+  while(i<=medium){
+    temp.push_back(res[i++]);
+  }
+  while(j<=high){
+    temp.push_back(res[j++]);
+  }
+
+  // Move back to the original array:
+  for(int i=low;i<high;i++){
+      res[i]=temp[i-low];
+  }
+}
+void mergeSort(int low, int high, vector<int>res, int n){
+  if(low==high)return;
+    int medium=low+high;
+    mergeSort(low, medium, res, n);
+    mergeSort(medium+1, high, res, n);
+    mergetwoSortedArray(low, medium, high, res);
+}
+vector<int>countInversion(vector<int>&res, int n){
+  vector<vector<int>>vt;
+  int low=0, high=n-1;
+  mergeSort(low, high, res, n);
+  return res;
+}
 int main(){
-  vector<int>res={2, 6, 0, 7, 0, 0, 3};
-  moveZeroes(res);
-  for(int i:res){
+  vector<int>res={5, 3, 2, 4, 1};
+  // the answer i want is: 5, 3, 5, 2, 5, 4, 5, 1.  3, 2), 3, 1). 2, 1.) 4, 1.
+  int n=res.size();
+  vector<int>out=countInversion(res, n);
+  for(int i:out){
     cout<<i<<" ";
   }
 return 0;
