9b6413c06041511f3a7c7f6f2b915fffb1466393
Dipesh Sharma <dipeshsharmagautam@outlook.com>
2025-11-07 20:29:36 +0530

    just minor tweak with the m color graph add which i've not written yet check again that topic for the graph.

---
+++ BackTracking/m-color-graph.cpp
@@ -0,0 +1,8 @@
+// First i've to understand what's reallyl mean the bipartide graph then i will only solve this m-color-graph.
+//ON the unorderd graph we've to find out how many color that we need max colors that same adj. veritices nodes don't have the same color.
+#include <iostream>
+using namespace std;
+int main(){
+    
+return 0;
+}
