826120347b62dad7e72a599a77cddcfa14c21132
Dipesh Sharma <dipeshsharmagautam@outlook.com>
2025-11-11 22:52:18 +0530

    just the revision of teh Arthmatic Expression, of the fix conversion. First is infix to postfix: just if teh element is the operand push to the result. if closing brack appear, just pop the element from the all operator till opeanign bracket appear. on any point if the new opeator comes lower value then the stack top of the highPriority just keep poping the elemtn from the stack and push it to the result.

--- Practice/test4.cpp
+++ Practice/test4.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+class Node{
+public:
+int data;
+Node* next;
+Node(int value): data(value), next(nullptr) {};
+};
+Node* convertArrayToLL(vector<int>arr){
+Node* head=new Node(arr[0]);
+Node* temp=head;
+for(int i=1;i<arr.size();i++){
+Node* eachValue=new Node(arr[i]);
+temp->next=eachValue;
+// Must move to next on Linked List:
+temp=eachValue;
+}
+return head;
+}
+void Display(Node* head){
+Node* temp=head;
+while(temp){
+cout<<temp->data<<" ";
+temp=temp->next;
+}
+cout<<endl;
+}
 int main(){
-   cout<<5/6;
+vector<int>arr={2, 9, 10, 1, 29, 30, 18, 50, 5, 21, 25, 30, 14, 35};
+Node* head=convertArrayToLL(arr);
+Display(head);
 
-return 0;
 }
