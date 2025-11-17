453a0291098f15dae9e4ba5541d032f6785a7907
Dipesh Sharma <dipeshsharmagautam@outlook.com>
2025-11-17 10:19:53 +0530

    This is the almost same approach to the infix to postfix, just two minor tweaks, is: first we reverse the given infix and after converting that to the postfix we again reverse now we get the prefix Expression.

--- Stack Queue/Arthmatic Expression/infix-prefix.cpp
+++ Stack Queue/Arthmatic Expression/infix-prefix.cpp
@@ -5,64 +5,77 @@ using namespace std;
 
 
 
-string reverseString(string &st){
-  int left=0, right=st.size()-1;
-  while(left<right){
+string reverseString(string& st) {
+  int left = 0, right = st.size() - 1;
+  while(left < right) {
+    if(st[left] == '(') st[left] = ')';
+    else if(st[left] == ')') st[left] = '(';
+
+    if(st[right] == '(') st[right] = ')';
+    else if(st[right] == ')') st[right] = '(';
+
     swap(st[right], st[left]);
     left++;
     right--;
   }
   return st;
 }
-bool isOperand(char s){
+bool isOperand(char s) {
   if(
-    (s>='A' && s<='Z' ) || 
-    (s>='a' && s<='z')||
-    (s>='0' && s<='9')
-  ){
+    (s >= 'A' && s <= 'Z') ||
+    (s >= 'a' && s <= 'z') ||
+    (s >= '0' && s <= '9')
+    ) {
     return true;
   }
-  else{
+  else {
     return false;
   }
 }
 
-int highPriority(char ch){
-  if(ch=='^'){
+int highPriority(char ch) {
+  if(ch == '^') {
     return 3;
   }
-  else if(ch=='/' ||ch=='*'){
+  else if(ch == '/' || ch == '*') {
     return 2;
   }
- else if(ch=='+' || ch=='-'){
+  else if(ch == '+' || ch == '-') {
     return 1;
   }
-  else{
+  else {
     return 0;
   }
 }
 
-string inToPre(string st){
+string inToPre(string st) {
   stack<char>s;
   reverseString(st);
-  string res="";
-for(int i=0;i<st.size();i++){
-  if(isOperand(st[i])){
+  string res = "";
+  for(int i = 0;i < st.size();i++) {
+    if(isOperand(st[i])){
     res+=st[i];
   }
+  else if(st[i]=='('){
+    s.push(st[i]);
+  }
+  // When close brack appear pop out all element and store on res.
  else if(st[i]==')'){
-      while(s.size() && s.top()!='('){
+      while(s.size() &&s.top()!='('){
           res+=s.top();
           s.pop();
       }
+      if(s.size()){
+        // Remove '(' from the stack. 
+        s.pop();
+      }
   }
- else if(highPriority(s.top())>highPriority(st[i])){
-    while(highPriority(s.top())>highPriority(st[i])){
+ else {
+    while(s.size() && highPriority(s.top())>highPriority(st[i])){
       res+=s.top();
       s.pop();
     }
-  }
-  else{
+    // Store appear operator.
     s.push(st[i]);
   }
 }
@@ -73,9 +86,9 @@ for(int i=0;i<st.size();i++){
   reverseString(res);
   return res;
 }
-int main(){
-   string st="a+b*(c^d-e)";
+int main() {
+  string st = "a+b*(c^d-e)";
   //  Result should be: +-*+abcdf
-   cout<<"Postfix Conversion is:"<<inToPre(st)<<endl;
-return 0;
+  cout << "Postfix Conversion is:" << inToPre(st) << endl;
+  return 0;
 }
