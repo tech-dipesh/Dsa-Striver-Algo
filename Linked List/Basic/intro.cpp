5aaf9b903f2d1209ec608986f4207951e47878df
Dipesh Sharma <dipeshsharmagautam@outlook.com>
2025-12-04 23:06:05 +0530

    Add the first file of teh linekd list, november/10 The intro file is just a bsic where i write about how the pointer is really work

---
+++ Linked List/Basic/intro.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+   int b=823;
+   int *c=&b;
+   cout<<c; 
+  //  Which it gives me the b memory location the same approach that we use on linked list.
+  // We can't store memory location rather we store teh pointer to the location.
+return 0;
+}
