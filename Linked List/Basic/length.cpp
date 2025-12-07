800c6c30d13f318218d7e0ad21f93883350ec812
Dipesh Sharma <dipeshsharmagautam@outlook.com>
2025-12-07 17:48:16 +0530

    Write the logic for find a length of the given linked list, with first create a convertArrayToLinkedList, and with lengthOfTheLL just keep counting the head is not null just keep count that's it

---
+++ Linked List/Basic/length.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+class Node{
+  public:
+  int data;
+  Node* next;
+  // Default Pointer:
+  Node(){
+    Node* head=nullptr;
+  }
+  Node(int data1){
+    data=data1;
+    next=nullptr;
+  }
+  Node(int data1, Node* data2){
+    data=data1;
+    next=data2;
+  }
+};
+
+  // where will be start:
+  Node* convertArraytoLinkedList(int arr[], int n){
+    Node* head=new Node(arr[0]);
+    Node* temp=head;
+    for(int i=1;i<n;i++){
+      // Node* temp=new Node(arr[i]);
+      Node *eachValue=new Node(arr[i]);
+      temp->next=eachValue;
+      temp=eachValue;
+    }   
+    return head;
+  }
+
+
+  int LengthOfTheLinkedList(Node* head){
+    int count=0;
+    Node* temp=head;
+    while(temp){
+      count++;
+    temp=temp->next;;
+    }
+    return count;
+  }
+  int main(){
+  int arr[]={3, 9, 10, 23, 93};
+  int n=sizeof(arr)/sizeof(arr[0]);
+  Node* head=convertArraytoLinkedList(arr, n);
+  Node* temp=head;
+  while(temp){
+    cout<<temp->data<<" ";
+    temp=temp->next;
+  }
+  cout<<endl<<LengthOfTheLinkedList(head);
+return 0;
+}
