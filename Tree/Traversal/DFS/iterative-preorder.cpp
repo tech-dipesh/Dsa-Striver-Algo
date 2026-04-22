a55335b865636a5819c63b3465e81205586f8f73
Dipesh Sharma <dipeshsharmagautam@outlook.com>
2026-04-22 21:26:42 +0530

    Add the Iterative approach of the Preorder with the code example

---
+++ Tree/Traversal/DFS/iterative-preorder.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+class Node {
+public:
+  int data;
+  Node* left;
+  Node* right;
+  // Constructor:
+  Node() : data(0), left(nullptr), right(nullptr) {};
+  Node(int value) : data(value), left(nullptr), right(nullptr) {};
+  Node(int value, Node* leftchild, Node* rightchild) : data(value), left(leftchild), right(rightchild) {};
+};
+void levelOrder(Node* root) {
+  vector<int>ans;
+  stack<Node*>st;
+  if(root == nullptr) {
+    return;
+  }
+  // First push root to the stac: and:
+  Node* temp = root;
+  st.push(temp);
+  while(st.size()) {
+    root = st.top();
+    st.pop();
+    ans.push_back(root->data);
+    if(root->right) st.push(root->right);
+    if(root->left) st.push(root->left);
+  }
+  for(int i : ans) {
+    cout << i << " ";
+  }
+}
+int main() {
+  Node* root = new Node(3);
+  root->left = new Node(7);
+  root->right = new Node(11);
+  root->left->left = new Node(2);
+  root->left->right = new Node(5);
+  root->right->left = new Node(8);
+  root->right->left = new Node(10);
+  
+  levelOrder(root);
+  return 0;
+}
